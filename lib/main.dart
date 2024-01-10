769099c18cba978104268c761bd57f252b1b6b3a
shiyajanet <janet98715@gmail.com>
2024-01-10 18:12:55 +0530

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "New Project",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Learn Flutter",
+            style: TextStyle(fontSize: 20),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blue,
+          leading: Icon(Icons.home),
+          actions: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+            IconButton(onPressed: () {}, icon: Icon(Icons.more_vert)),
+          ],
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30))),
+        ),
+        body: Center(
+          child: Text(
+            "Hello Shiya",
+            style: TextStyle(fontSize: 20, color: Colors.red),
+          ),
+        ),
+        drawer: Drawer(),
+      ),
+    );
+  }
+}
